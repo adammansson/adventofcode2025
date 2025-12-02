6e426bf19541eb9ac94aaaa392dfde949b617bae
Adam Månsson <adam.s.mansson@gmail.com>
2025-12-02 17:45:56 +0100

    improve scala day02

--- scala/day02.scala
+++ scala/day02.scala
@@ -1,21 +1,33 @@
 //> using toolkit default
 
-def base(f: String => Boolean): Long =
+def base(pred: String => Boolean): Long =
   os.read(os.pwd / "input" / "day02" / "in.txt")
     .strip
     .split(",")
-    .map(r => (r.split("-").head).toLong to (r.split("-").last).toLong)
-    .map(r => r.filter(n => f(n.toString)).sum)
+    .map(_.trim)
+    .collect { case s"$start-$end" => start.toLong to end.toLong }
+    .flatMap(range =>
+      range.view
+        .map(_.toString)
+        .filter(pred)
+        .map(_.toLong)
+    )
     .sum
 
 def partOne(): Long =
-  base(s => s.take(s.length / 2) == s.drop(s.length / 2))
+  base(s =>
+    val mid = s.length / 2
+    s.length % 2 == 0 && s.take(mid) == s.drop(mid)
+  )
 
 def partTwo(): Long =
   base(s =>
-    (1 to s.length / 2).exists(gs =>
-      s.grouped(gs).forall(g => g == s.grouped(gs).next)
-    )
+    (1 to s.length / 2)
+      .filter(gs => s.length % gs == 0)
+      .exists(gs =>
+        val seq = s.take(gs)
+        s.grouped(gs).forall(_ == seq)
+      )
   )
 
 @main def run(): Unit =
